d72273b67fce81b4e66fc629eb88d1987c9d3ffa
45panov <khorkov.anton@gmail.com>
2023-03-30 22:20:48 +0300

    Chapter 11. Source of Evil

--- 01_multi_currency.py
+++ 01_multi_currency.py
@@ -10,32 +10,16 @@ class Money:
             return False
 
     def dollar(amount):
-        return Dollar(amount, 'USD')
+        return Money(amount, 'USD')
 
     def franc(amount):
-        return Franc(amount, 'CHF')
+        return Money(amount, 'CHF')
 
     def currency(self):
         return self.curr
 
     def times(self, multiplier):
         return Money(self._amount * multiplier, self.curr)
-
-
-class Dollar(Money):
-    def __init__(self, amount, curr=None):
-        super().__init__(amount, curr)
-
-    # def times(self, multiplier):
-    #     return Money(self._amount * multiplier, self.curr)
-
-
-class Franc(Money):
-    def __init__(self, amount, curr=None):
-        super().__init__(amount, curr)
-
-    # def times(self, multiplier):
-    #     return Money(self._amount * multiplier, self.curr)
 
 
 def test_multiplication():
@@ -49,29 +33,26 @@ def test_multiplication():
 
 
 def test_equality():
-    assert Dollar(5) == Dollar(5), "Dollar(5) == Dollar(5) must be True!"
-    assert not Dollar(5) == Dollar(6), "Dollar(5) == Dollar(6) must be False!"
+    assert Money.dollar(5) == Money.dollar(5), "Dollar(5) == Dollar(5) must be True!"
+    assert not Money.dollar(5) == Money.dollar(6), "Dollar(5) == Dollar(6) must be False!"
 
-    assert Franc(5) == Franc(5), "Franc(5) == Franc(5) must be True!"
-    assert not Franc(5) == Franc(6), "Franc(5) == Franc(6) must be False!"
-    assert not Franc(5, 'CHF') == Dollar(5, 'USD'), "5 Francs are not equal to 5 Dollars"
+    assert Money.franc(5) == Money.franc(5), "Franc(5) == Franc(5) must be True!"
+    assert not Money.franc(5) == Money.franc(6), "Franc(5) == Franc(6) must be False!"
+    assert not Money.franc(5) == Money.dollar(5), "5 Francs are not equal to 5 Dollars"
 
 
 def test_franc_multiplication():
-    five = Franc(5)
-    assert Franc(10) == five.times(2), "Franc(5) == Franc(5) must be True!"
-    assert Franc(15) == five.times(3), "Franc(5) == Franc(6) must be False!"
+    five = Money.franc(5)
+    assert Money.franc(10) == five.times(2), "Franc(5) == Franc(5) must be True!"
+    assert Money.franc(15) == five.times(3), "Franc(5) == Franc(6) must be False!"
 
 
 def test_currency():
     assert 'USD' == Money.dollar(1).currency(), "Money.dollar(1).currency() must be equal to 'USD'"
     assert 'CHF' == Money.franc(1).currency(), "Money.franc(1).currency() must be equal to 'CHF'"
 
-def test_different_class_equality():
-    assert Money(10, 'CHF') == Franc(10, 'CHF'), "Object Money(10, 'CHF') must be equal to Franc(10, 'CHF')"
 
 test_multiplication()
 test_equality()
 test_franc_multiplication()
 test_currency()
-test_different_class_equality()
