95693e10050ab4116b4463e9375d609dade710ed
45panov <khorkov.anton@gmail.com>
2023-04-01 19:51:07 +0300

    Chapter13. Making implementation real.

--- 01_multi_currency.py
+++ 01_multi_currency.py
@@ -1,5 +1,31 @@
-class Money:
-    def __init__(self, amount: int, curr: str):
+class Expression():
+    def __add__(self, other):
+        augend = self
+        addend = other
+        return Sum(augend, addend)
+
+    def reduce(self, to):
+        return self
+
+
+class Sum(Expression):
+    def __init__(self, augend, addend):
+        self.augend = augend
+        self.addend = addend
+
+    def reduce(self, to):
+        amount = self.augend._amount + self.addend._amount
+        return Money(amount, to)
+
+
+class Bank:
+    def reduce(self, source, to: str):
+        self.sum = source
+        return self.sum.reduce(to)
+
+
+class Money(Expression):
+    def __init__(self, amount, curr: str):
         self._amount = amount
         self.curr = curr
 
@@ -8,6 +34,9 @@ class Money:
             return True
         else:
             return False
+
+    def __mul__(self, multiplier):
+        return Money(self._amount * multiplier, self.curr)
 
     def dollar(amount):
         return Money(amount, 'USD')
@@ -18,18 +47,15 @@ class Money:
     def currency(self):
         return self.curr
 
-    def times(self, multiplier):
-        return Money(self._amount * multiplier, self.curr)
-
 
 def test_multiplication():
     five = Money.dollar(5)
-    assert Money.dollar(10) == five.times(2), "False!"
-    assert Money.dollar(15) == five.times(3), "False!"
+    assert Money.dollar(10) == five * 2, "Money.dollar(10) must be equal to five * 2"
+    assert Money.dollar(15) == five * 3, "Money.dollar(15) must be equal to five * 3"
 
     six = Money.franc(6)
-    assert Money.franc(12) == six.times(2), "False!"
-    assert Money.franc(18) == six.times(3), "False!"
+    assert Money.franc(12) == six * 2, "Money.franc(12) must be equal to six * 2"
+    assert Money.franc(18) == six * 3, "Money.franc(18) must be equal to six * 3"
 
 
 def test_equality():
@@ -43,8 +69,8 @@ def test_equality():
 
 def test_franc_multiplication():
     five = Money.franc(5)
-    assert Money.franc(10) == five.times(2), "Franc(5) == Franc(5) must be True!"
-    assert Money.franc(15) == five.times(3), "Franc(5) == Franc(6) must be False!"
+    assert Money.franc(10) == five * 2, "Franc(5) == Franc(5) must be True!"
+    assert Money.franc(15) == five * 3, "Franc(5) == Franc(6) must be False!"
 
 
 def test_currency():
@@ -52,7 +78,43 @@ def test_currency():
     assert 'CHF' == Money.franc(1).currency(), "Money.franc(1).currency() must be equal to 'CHF'"
 
 
+def test_simple_addition():
+    sum = Money.dollar(5) + Money.dollar(5)
+    assert Money.dollar(10) == sum, "Money.dollar(5) + Money.dollar(5) must be an equal to Money.dollar(10)"
+
+    five = Money.dollar(5)
+    sum = five + five
+    bank = Bank()
+    reduced = bank.reduce(sum, 'USD')
+    assert Money.dollar(10) == reduced, "Money.dollar must be equal to reduced!"
+
+
+def test_plus_returns_sum():
+    five = Money.dollar(5)
+    result = five + five
+    sum = result
+    assert five == sum.augend, "five must be equal to sum.augend"
+    assert five == sum.addend, "five must be equal to sum.addend"
+
+
+def test_reduce_sum():
+    sum = Sum(Money.dollar(3), Money.dollar(4))
+    bank = Bank()
+    result = bank.reduce(sum, 'USD')
+    assert Money.dollar(7) == result, "Money.dollar(7) must be equal to result"
+
+
+def test_reduce_money():
+    bank = Bank()
+    result = bank.reduce(Money.dollar(1), 'USD')
+    assert Money.dollar(1) == result, "Money.dollar(1) must be equal to result"
+
+
 test_multiplication()
 test_equality()
 test_franc_multiplication()
 test_currency()
+# test_simple_addition()
+test_plus_returns_sum()
+test_reduce_sum()
+test_reduce_money()
