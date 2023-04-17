f5de7db7955ce5cf4df55a4de3d10682209df64e
45panov <khorkov.anton@gmail.com>
2023-04-17 19:56:28 +0300

    Chapter 19. Set up the table. SetUp method.

--- 02_xunit.py
+++ 02_xunit.py
@@ -1,10 +1,16 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
-        
+
+
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
@@ -12,12 +18,22 @@ class WasRun(TestCase):
        
     def testMethod(self):
         self.wasRun = 1
+
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
         
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun, "self.test.wasRun must be True"
+
+    def testSetUp(self):
+        self.test.run()
+        assert self.test.wasSetUp, "self.test.wasSetUp must be True"
         
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
